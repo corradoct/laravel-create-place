2bf78d4216ce6b992abf7fcf2ec28f3428df5c7d
Corrado <corradoct@hotmail.it>
2020-09-01 16:42:07 +0200

    Ho stampato a video

--- resources/views/students.blade.php
+++ resources/views/students.blade.php
@@ -4,4 +4,16 @@
 
   <h1>Studenti</h1>
 
+  @foreach ($students as $student)
+
+    <div>
+      <h2>Nome studente : {{ $student->nome }} {{ $student->cognome }}</h2>
+      <ul>
+        <li>Classe : {{ $student->classe }}</li>
+        <li>Voto : {{ $student->voto }}</li>
+      </ul>
+    </div>
+
+  @endforeach
+
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/students', 'StudentController@index');
+Route::get('/students', 'StudentController@index')->name('students');
